41cb24b260c6b2d2f5763797e27c69b5c3e54ad4
troy452 <miralbenizi7@gmail.com>
2026-03-23 10:45:01 +0100

    Update products.js

--- products.js
+++ products.js
@@ -2,12 +2,13 @@ const allProducts = [
   {
     id: 1,
     category: "Montessori",
-    title: "Educational Montessori Toy Multi-Layer Track Adventure",
+    title: "Educational Montessori Toy Multi-Layer Track Adventure Game",
     price: "49.66",
-    description: "An interactive brain-building game featuring a multi-layer rail rolling ball model.",
+    description: "An interactive brain-building game featuring a multi-layer rail rolling ball model. Designed to enhance motor skills.",
     images: [
-      "Montessori/1.jpg", 
-      "Montessori/2.jpg"
+      "Montessori/1.avif", 
+      "Montessori/2.avif",
+      "Montessori/3.avif"
     ],
     buyLink: "https://s.click.aliexpress.com/e/_c3nfADV1"
   }
