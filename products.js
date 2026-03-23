b86518cd034b81100217f50356f1f20a69bb90e5
troy452 <miralbenizi7@gmail.com>
2026-03-23 10:48:50 +0100

    Update products.js

--- products.js
+++ products.js
@@ -4,12 +4,25 @@ const allProducts = [
     category: "Montessori",
     title: "Educational Montessori Toy Multi-Layer Track Adventure Game",
     price: "49.66",
-    description: "An interactive brain-building game featuring a multi-layer rail rolling ball model. Designed to enhance motor skills.",
+    description: "An interactive brain-building game featuring a multi-layer rail rolling ball model. Designed to enhance motor skills and logical thinking.",
     images: [
       "Montessori/1.avif", 
       "Montessori/2.avif",
       "Montessori/3.avif"
     ],
     buyLink: "https://s.click.aliexpress.com/e/_c3nfADV1"
+  },
+  {
+    id: 2,
+    category: "Montessori",
+    title: "Montessori Busy Board for Toddlers 1-3 | Sensory Music & Light",
+    price: "48.72",
+    description: "A comprehensive sensory toy featuring music and lights. Perfect for developing fine motor skills and providing early learning engagement for toddlers.",
+    images: [
+      "Montessori/4.avif", 
+      "Montessori/5.avif",
+      "Montessori/6.avif"
+    ],
+    buyLink: "https://s.click.aliexpress.com/e/_c4L0PDxz"
   }
 ];
