a2fb329d94e80eba52dca82ab40e277b4b56fe34
troy452 <miralbenizi7@gmail.com>
2026-03-23 10:54:03 +0100

    Update products.js

--- products.js
+++ products.js
@@ -2,27 +2,31 @@ const allProducts = [
   {
     id: 1,
     category: "Montessori",
+    age: "3+ Years",
     title: "Educational Montessori Toy Multi-Layer Track Adventure Game",
     price: "49.66",
     description: "An interactive brain-building game featuring a multi-layer rail rolling ball model. Designed to enhance motor skills and logical thinking.",
-    images: [
-      "Montessori/1.avif", 
-      "Montessori/2.avif",
-      "Montessori/3.avif"
-    ],
+    images: ["Montessori/1.avif", "Montessori/2.avif", "Montessori/3.avif"],
     buyLink: "https://s.click.aliexpress.com/e/_c3nfADV1"
   },
   {
     id: 2,
     category: "Montessori",
+    age: "1-3 Years",
     title: "Montessori Busy Board for Toddlers 1-3 | Sensory Music & Light",
     price: "48.72",
     description: "A comprehensive sensory toy featuring music and lights. Perfect for developing fine motor skills and providing early learning engagement for toddlers.",
-    images: [
-      "Montessori/4.avif", 
-      "Montessori/5.avif",
-      "Montessori/6.avif"
-    ],
+    images: ["Montessori/4.avif", "Montessori/5.avif", "Montessori/6.avif"],
     buyLink: "https://s.click.aliexpress.com/e/_c4L0PDxz"
+  },
+  {
+    id: 3,
+    category: "Montessori",
+    age: "2-3 Years",
+    title: "Busy Board Montessori Toys for Toddlers | Fine Motor Skills",
+    price: "49.96",
+    description: "A developmental powerhouse designed specifically for toddlers aged 2-3. Focuses on fine motor skill development and tactile educational learning through various activities.",
+    images: ["Montessori/7.avif", "Montessori/8.avif", "Montessori/9.avif"],
+    buyLink: "https://s.click.aliexpress.com/e/_c45YKcmn"
   }
 ];
